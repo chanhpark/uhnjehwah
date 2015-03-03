71d9aa2743a8434d1b7354efc35bfe2514fa6539
chanhpark <chanhyun.park1211@gmail.com>
2015-03-03 09:26:54 +0900

    add other lines functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'alerts' => 'alerts#index'
   get 'red' => 'red#index'
   get 'red/:id' => 'red#show'
+  get 'orange' => 'orange#index'
+  get 'orange/:id' => 'orange#show'
   get 'blue' => 'blue#index'
   get 'blue/:id' => 'blue#show'
+  get 'green' => 'green#index'
+  get 'green/:id' => 'green#show'
   devise_for :users
 end
