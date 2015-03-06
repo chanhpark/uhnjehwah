71d68045d55db961a584a6b4bc8c0d66353f7445
chanhpark <chanhyun.park1211@gmail.com>
2015-03-06 12:23:23 +0900

    convenstions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  get 'alerts' => 'alerts#index'
-  get 'red' => 'red#index'
-  get 'red/:id' => 'red#show'
-  get 'orange' => 'orange#index'
-  get 'orange/:id' => 'orange#show'
-  get 'blue' => 'blue#index'
-  get 'blue/:id' => 'blue#show'
-  get 'green' => 'green#index'
-  get 'green/:id' => 'green#show'
   devise_for :users
-end
+  resources :trains
+  resources :routes
+  resources :stops
+
+  namespace :admin do
+    resources :trains
+    resources :routes
+    resources :stops
+  end
